f51388c1b6b951b938b50c63f64056a61a9feeb2
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2022-01-06 11:59:16 +0700

    copy

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -14,5 +14,12 @@
                 </a>
             </li>
         </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Ridlo Suhardi with CI 4</span>
+            <a class="link-secondary" href="#" aria-label="Add a new report">
+                <span data-feather="alert-circle"></span>
+            </a>
+        </h6>
     </div>
 </nav>
